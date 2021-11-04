fadcae8887a4b6b4ccde66278da22eced6cd976e
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-11-04 09:46:09 -0400

    Add files via upload

--- P1tabledata.sql
+++ P1tabledata.sql
@@ -6,12 +6,14 @@ create table clients
     primary key (clientID)
 );
 
-drop table clients cascade;
+-- drop table clients cascade;
+
+-- insert into clients (clientName, clientEmail) values
+-- ('James Embrey', 'james.embrey@revature.net'), ('Semaj Yember', 'semaj.yember@revature.net');
 
 select * from clients;
 
 -- delete from clients;
-
 
 
 -- CHECK AGAIN v
@@ -19,12 +21,15 @@ create table shoppinglist
 (
     itemID serial,
     clientID bigserial,
-    productName varchar(255),
+    productName varchar(255) not null,
     primary key(itemID),
     foreign key(clientID) references clients(clientID)
 );
 
-drop table shoppinglist cascade;
+-- drop table shoppinglist cascade;
+
+-- insert into shoppinglist (productName) VALUES
+-- ("Eggs"), ("Samsung S9000"); 
 
 select * from shoppinglist;
 
@@ -32,61 +37,125 @@ select * from shoppinglist;
 create table expenses
 (
     lineitemID serial,
-    subtotal decimal(10, 2),
-    -- orderID not null,
+    -- orderID int not null,
+    -- itemID int not null,
+    subtotal decimal(10, 2) not null,
+    price decimal(10, 2) not null,
     primary key(lineitemID)
+    -- foreign key(itemID) references shoppinglist(itemID)
+    -- foreign key(orderID) references orders(orderID)
 );
 
-select * from expenses;
-
-drop table expenses cascade;
+-- drop table expenses cascade;
 
 
 create table orders
 (
-    orderSetID serial,
-    -- clientID int not null,
-    -- productName varchar(255),
-    total decimal(13, 2),
-    -- orderDate DATETIME
-    primary key (orderSetID)
-    -- foreign key (clientID) references clients(clientID)
+    orderID serial,
+    total decimal(13, 2) not null,
+    primary key (orderID),
+    foreign key (clientID) references clients(clientID)
 );
 
-select * from orders;
+-- drop table orders cascade;
 
-drop table orders cascade;
+-- insert into orders (total) VALUES
+-- (12.07);
 
 
 create table vendors
 (
-    vendorID int not null,
-    vendorName varchar(100),
-    vendorProduct varchar(100),
+    vendorID serial primary key,
+    vendorName varchar(255),
+    vendorProduct varchar(255),
     vendorCost decimal(10, 2),
-    vendorQuantity numeric(100),
-    primary key (vendorID)
+    vendorQuantity numeric(100)
 );
 
-select * from vendors;
+insert into vendors(vendorName, vendorProduct, vendorCost, vendorQuantity) VALUES
+('EEFoods', 'Bacon', 5.50, 100), ('EEFoods', 'Cinnamon Toast Crunch', 2.50, 100), ('EEFoods', 'Eggs', 1.60, 100), ('EEEntertainment', 'Dark Side of the Moon Vinyl', 24.99, 100), ('EEEntertainment', 'Fight Club', 7.00, 100), ('EEEntertainment', 'Zelda Breath of the Wild', 39.99, 100), ('EETechnology', 'Oculus Quest 2', 300.00, 100), ('EETechnology', 'Samsung S9000', 900.00, 100), ('EETechnology', 'Tile Mate', 24.99, 100);
 
-insert into vendors(vendorID, vendorName, vendorProduct, vendorCost, vendorQuantity) VALUES
-(1, 'Pega', 'Steeleye Stout', 18.00, 100), (2, 'Pega', 'Louisiana Fiery Hot Pepper Sauce', 21.06, 100), (3, 'EEFood', 'Bacon', 5.50, 0), (4, 'EEFood', 'Cinnamon Toast Crunch', 2.50, 100), (5, 'EEFood', 'Eggs', 1.60, 100), (6, 'EEEntertainment', 'Dark Side of the Moon Vinyl', 24.99, 100), (7, 'EEEntertainment', 'Fight Club', 7.00, 100), (8, 'EEEntertainment', 'Zelda Breath of the Wild', 39.99, 100), (9, 'EETechnology', 'Oculus Quest 2', 300.00, 100), (10, 'EETechnology', 'Samsung S9000', 900.00, 100), (11, 'EETechnology', 'Tile Mate', 24.99, 100);
 
--- UPDATE vendors SET vendorQuantity = 100 WHERE vendorQuantity = 99;
+-- drop table vendors cascade;
 
--- UPDATE vendors SET vendorQuantity = 0 WHERE vendorQuantity = 99 AND vendorProduct = 'Steeleye Stout';
-
-drop table vendors cascade;
-
--- create table vendorinventory
--- (
---     vendorInvID int not null,
---     vendorName varchar(255) not null,
---     vendorItem varchar(255) not null,
---     vendorCost numeric(3, 2) not null,
---     primary key (vendorInvID),
---     foreign key (vendorID) references vendors(vendorID)
--- );
+create table vendorinventory
+(
+    vendorInvID serial primary key,
+    vendorName varchar(255) not null,
+    vendorItem varchar(255) not null,
+    vendorCost decimal(3, 2) not null,
+    foreign key (vendorName) references vendors(vendorName)
+);
 
 -- drop table vendorinventory cascade;
+
+
+create table notFoundTable
+(
+    notFoundID serial primary key,
+
+);
+
+
+
+-- select * from clients
+
+-- drop table clients
+
+-- DELETE FROM Clients
+-- WHERE id = 1;
+
+-- drop table vendors
+
+-- insert into vendors (vendorName, vendorProduct, vendorCost, vendorQuantity) values
+
+
+-- create table EarlyBirdProduce
+-- (
+--     id serial primary key,
+--     productName varchar(100),
+--     productCost decimal(10, 2),
+--     productQuantity numeric(100)
+-- );
+
+-- insert into EarlyBirdProduce (productName, productCost, productQuantity) values
+-- ('Eggs', 1.60, 100), ('Sliced Bacon', 5.50, 100), ('Cinnamon Toast Crunch', 2.50, 100); 
+
+-- select * from EarlyBirdProduce
+
+-- DELETE FROM EarlyBirdProduce
+-- WHERE id = 4;
+
+-- drop table EarlyBirdProduce
+
+
+-- create table TheEntertainer
+-- (
+--     id serial primary key,
+--     productName varchar(100),
+--     productCost decimal(10, 2),
+--     productQuantity numeric(100)
+-- );
+
+-- insert into TheEntertainer (productName, productCost, productQuantity) values
+-- ('Fight Club', 7.00, 100), ('Zelda BOTW', 39.99, 100), ('Dark Side of the Moon Vinyl', 24.99, 100);
+
+-- select * from TheEntertainer
+
+-- drop table TheEntertainer
+
+
+-- create table Technotrocity
+-- (
+--     id serial primary key,
+--     productName varchar(100),
+--     productCost decimal(10, 2),
+--     productQuantity numeric(100)
+-- );
+
+-- insert into Technotrocity (productName, productCost, productQuantity) values
+-- ('Samsung S9000', 900.00, 100), ('Tile Mate', 24.99, 100), ('Oculus Quest 2', 300.00, 100);
+
+-- select * from Technotrocity
+
+-- drop table Technotrocity
--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 function buy(name) {
     debugger;
-    // find me the element that matches this conditions (i.e a tbody child element of another element with id='cart')
-    let cart = document.querySelector('#cart tbody');
-    // appends a row to the tbody element
-    let newItem = cart.insertRow();
-
-    let itemName = newItem.insertCell(0);
-    itemName.innerHTML = name;
+    let table = document.querySelector("#cart tbody");
+    let tableRow = table.insertRow();
+    
+    let productName = tableRow.insertCell(0);
+    productName.innerHTML = name;
 
     let elementId = '#' + name.replace(' ', '');
-    
-    let itemPrice = newItem.insertCell(1);
-    itemPrice.innerHTML = document.querySelector(elementId + ' .price').innerHTML;
 
-    let itemQuantity = newItem.insertCell(2);
-    itemQuantity.innerHTML = document.querySelector(elementId + ' td .quantity').value;
+    let price = tableRow.insertCell(1);
+    price.innerHTML = document.querySelector(elementId + ' .price').innerHTML;
 
-    //resets the input value so the quantity of the original order doesn't "stick"
-    document.querySelector(elementId + ' td .quantity').value = '';
+    let quantity = tableRow.insertCell(2);
+    quantity.innerHTML = document.querySelector(elementId + ' input').value;
+
+    let subTotal = tableRow.insertCell(3);
+    subTotal.innerHTML = document.querySelector(elementId + ' input').value * document.querySelector(elementId + ' .price').innerHTML.replace('$', '');
+
+    document.querySelector('#' + name.replace(' ', '') + ' input').value = '';
+    document.querySelector('#empty').innerHTML = '';
+}
+
+
+function checkout()
+{
+    alert("Thank you for shopping at Embrey Enterprises! Have a wonderful day!");
+    document.querySelectorAll("#cart tbody tr").forEach(row => row.remove());
+    document.querySelector('#empty').innerHTML = 'No Items in cart';
 }
